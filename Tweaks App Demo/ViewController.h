8b972eb1e93feada729ff7fed73711b43c67fb4b
Ivan Damjanović <ivan.damjanovic@infinum.hr>
2015-04-23 12:35:40 +0200

    Initial Commit

---
+++ Tweaks App Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tweaks App Demo
+//
+//  Created by Dama on 23/04/15.
+//  Copyright (c) 2015 Infinum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Tweaks App Demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Tweaks App Demo
+//
+//  Created by Dama on 23/04/15.
+//  Copyright (c) 2015 Infinum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
